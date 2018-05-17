a46567730117778f6a230cf384b9b5b528752863
yoojia.chen <yoojiachen@gmail.com>
2018-05-17 16:16:46 +0800

    添加DropIndexBuilder

---
+++ sqlx/drop_index_test.go
@@ -0,0 +1,14 @@
+package sqlx
+
+import "testing"
+
+//
+// Author: 陈永佳 chenyongjia@parkingwang.com, yoojiachen@gmail.com
+//
+
+func TestDropIndex(t *testing.T) {
+	sql := DropIndex("idx_Uid").
+		OnTable("t_username").
+		GetSQL()
+	checkSQLMatches(sql, "ALTER TABLE `t_username` DROP INDEX `idx_Uid`;", t)
+}
